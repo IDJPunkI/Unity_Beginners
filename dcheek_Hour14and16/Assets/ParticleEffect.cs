e10e944ce8ce4ae8bad3deb16b9a7a91024ae772
IDJPunkI <dj2334@hotmail.com>
2024-05-03 21:40:04 -0700

    Hour 14 & 16 Completed

---
+++ dcheek_Hour14and16/Assets/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/*
+ * 1) Start Speed set to 15.
+ * 2) Start size set to 50.
+ * 3) Start color set to black.
+ * 4) Emission rate over time set to 7.
+ * 5) Shape set to sphere.
+ * 6) Max particles set to 5000.
+ */
